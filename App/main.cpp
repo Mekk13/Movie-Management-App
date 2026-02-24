8acd83874a7037bf6bc52ec643980bb72a19ad64
Mekk13 <mariasuciu313@gmail.com>
2026-02-24 12:58:10 -0800

    Added Movie Management App

---
+++ App/main.cpp
@@ -0,0 +1,11 @@
+#include "App.h"
+#include <QtWidgets/QApplication>
+// #include "ui_FileFormatDialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    A89 w;
+    w.show();
+    return a.exec();
+}
